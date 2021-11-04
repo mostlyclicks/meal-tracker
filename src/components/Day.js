a7f01702c47c12e9aae24825f7aafc4675f79fdd
mostlyclicks <carlos@mostlyclicks.com>
2021-11-04 14:04:40 -0600

    getting fancy with the cards

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Day = () => {
   return (
-    <div className="flex w-1/3 border-2 border-black text-center flex-col p-3">
-      <p>Monday, Nov 1st</p>
-      <h3>2784</h3>
-      <small>calories</small>
+    <div className="flex w-1/3 text-center flex-col p-3 rounded-md shadow-lg bg-white">
+      <p className="uppercase text-xs text-gray-500">Monday, Nov 1st</p>
+      <h3 className="text-2xl md:text-3xl">2784</h3>
+      <small className="text-gray-500">calories</small>
     </div>
   )
 }
--- src/components/Days.js
+++ src/components/Days.js
@@ -3,7 +3,7 @@ import Day from './Day'
 
 const Days = () => {
   return (
-    <div className="container border-solid border-2 border-black-400 mx-auto flex flex-wrap gap-4 justify-center p-4">
+    <div className="container border-solid border-2 border-black-400 mx-auto flex flex-wrap gap-6 justify-center p-4 bg-gray-50 rounded-2xl">
       <h1 className="w-full">Days Container</h1>
       <Day />
       <Day />
