d339087cddbcae9a882d2f7e4abec6af0feacde9
mostlyclicks <carlos@mostlyclicks.com>
2021-11-04 11:08:12 -0600

    add Day component

---
+++ src/components/Day.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Day = () => {
+  return (
+    <div style={{border:'1px solid black', textAlign:'center', padding:'3rem', width:'200px', height: '200px'}}>
+      <p>Monday, Nov 1st</p>
+      <h3>2784</h3>
+      <small>calories</small>
+    </div>
+  )
+}
+
+export default Day
--- src/components/Days.js
+++ src/components/Days.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Day from './Day'
 
 const Days = () => {
   return (
-    <div style={{border:'1px solid black', width: "80%", height: "80vh"}}>
+    <div style={{border:'1px solid black', width: "80%", height: "auto", display:'flex'}}>
       <h1>Days Container</h1>
-      
+      <Day />
+      <Day />
+      <Day />
+      <Day />
+      <Day />
+      <Day />
     </div>
   )
 }
